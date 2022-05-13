63e8fc8e86de3a33dfd5aeab6bbe0808e9d89be5
Jaya8277 <jayavish423@gmail.com>
2022-05-13 20:17:38 +0530

    Cards React bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Cards from './components/Cards.jsx';
+// import {a} from "react-router-dom";
+const App = () => {
+  return (
+    <React.Fragment>
+   <Cards />
+    </React.Fragment>
+  )
+}
+
+export default App;
